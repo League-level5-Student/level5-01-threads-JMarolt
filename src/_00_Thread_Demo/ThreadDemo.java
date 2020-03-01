6d7495d4b6fff75e03e8adaede727183c0cc298f
league <league@imac-18.attlocal.net>
2020-03-01 15:57:22 -0800

    learning threads

--- src/_00_Thread_Demo/ThreadDemo.java
+++ src/_00_Thread_Demo/ThreadDemo.java
@@ -1,6 +1,8 @@
 package _00_Thread_Demo;
 
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ThreadDemo {
@@ -8,6 +10,7 @@ public class ThreadDemo {
 			Robot timmy = new Robot(400, 700);
 			Robot tammy = new Robot(800, 700);
 			Robot sammy = new Robot(1200, 700);
+			ThreadDemo d = new ThreadDemo();
 			
 			timmy.setSpeed(10);
 			tammy.setSpeed(10);
@@ -22,12 +25,89 @@ public class ThreadDemo {
 			sammy.moveTo(1200, 700);
 			
 			Thread r1 = new Thread(()->timmy.move(400));
-			Thread r2 = new Thread(()->tammy.move(400));
-			Thread r3 = new Thread(()->sammy.move(400));
+			Thread r2 = new Thread(()->{tammy.move(400);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			});
+			Thread r3 = new Thread(()->{sammy.move(400);
+			sammy.setSpeed(100);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.setPenColor(Color.red);
+			sammy.setPenColor(Color.orange);
+			sammy.setPenColor(Color.yellow);
+			sammy.setPenColor(Color.green);
+			sammy.setPenColor(Color.blue);
+			sammy.setPenColor(Color.magenta);
+			sammy.setPenColor(Color.red);
+			sammy.setPenColor(Color.orange);
+			sammy.setPenColor(Color.yellow);
+			sammy.setPenColor(Color.green);
+			sammy.setPenColor(Color.blue);
+			sammy.setPenColor(Color.magenta);
+			sammy.setPenColor(Color.red);
+			sammy.setPenColor(Color.orange);
+			sammy.setPenColor(Color.yellow);
+			sammy.setPenColor(Color.green);
+			sammy.setPenColor(Color.blue);
+			sammy.setPenColor(Color.magenta);});
+			
+			//Thread r4 = new Thread(()->d.getClass());
+			//r4.
 			
 			r1.start();
 			r2.start();
 			r3.start();
-			
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-10880);
+			tammy.turn(10800);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			tammy.turn(-1080);
+			sammy.setSpeed(100);
+			sammy.penDown();
+			sammy.setPenColor(Color.red);
+			sammy.setPenColor(Color.orange);
+			sammy.setPenColor(Color.yellow);
+			sammy.setPenColor(Color.green);
+			sammy.setPenColor(Color.blue);
+			sammy.setPenColor(Color.magenta);
+			sammy.setPenColor(Color.red);
+			sammy.setPenColor(Color.orange);
+			sammy.setPenColor(Color.yellow);
+			sammy.setPenColor(Color.green);
+			sammy.setPenColor(Color.blue);
+			sammy.setPenColor(Color.magenta);
+			sammy.setPenColor(Color.red);
+			sammy.setPenColor(Color.orange);
+			sammy.setPenColor(Color.yellow);
+			sammy.setPenColor(Color.green);
+			sammy.setPenColor(Color.blue);
+			sammy.setPenColor(Color.magenta);
+			sammy.turn(10800);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.turn(-1080);
+			sammy.move(-450);
 	}
 }
--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,57 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	public static void main(String[] args) {
+		Robot one = new Robot(100,100);
+		Robot two = new Robot(300,100);
+		Robot three = new Robot(500,100);
+		Robot four = new Robot(200,300);
+		Robot five = new Robot(400,300);
+		Thread t1 = new Thread(()->{one.penDown();
+		one.setPenColor(Color.blue);
+		one.setSpeed(10);
+		for(int i = 0; i < 360; i ++) {
+		one.move(2);
+		one.turn(1);}
+		one.hide();});
+		Thread t2 = new Thread(()->{two.penDown();
+		two.setPenColor(Color.black);
+		two.setSpeed(10);
+		for(int i = 0; i < 360; i ++) {
+		two.move(2);
+		two.turn(1);}
+		two.hide();});
+		Thread t3 = new Thread(()->{three.penDown();
+		three.setPenColor(Color.red);
+		three.setSpeed(10);
+		for(int i = 0; i < 360; i ++) {
+		three.move(2);
+		three.turn(1);}
+		three.hide();});
+		Thread t4 = new Thread(()->{four.penDown();
+		four.setPenColor(Color.yellow);
+		four.setSpeed(10);
+		for(int i = 0; i < 360; i ++) {
+		four.move(2);
+		four.turn(1);}
+		four.hide();});
+		Thread t5 = new Thread(()->{five.penDown();
+		five.setPenColor(Color.green);
+		five.setSpeed(10);
+		for(int i = 0; i < 360; i ++) {five.move(2);
+		five.turn(1);}
+		five.hide();});
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+	}
+	
 }
 
